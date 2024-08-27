5a1546a17ca24d23e5070bca36f8b67acbeb10f2
taka012345 <117965802+taka012345@users.noreply.github.com>
2024-08-27 21:43:01 +0900

    Update and rename to responsive.css

--- index.js
+++
@@ -1,6 +0,0 @@
-let username;
-
-document.getElementById("mySubmit").onclick = function(){
-    username = document.getElementById("myText").value;
-    document.getElementById("myH1").textContent = `Hello ${username}`
-}
